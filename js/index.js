5638e556aea4fc8b2cafb3334f24d0dfcb6dacf2
gev_org <thecarguyarm@gmail.com>
2023-06-03 19:09:00 +0400

    updates

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@ function showConfirm() {
     confirm("Do you confirm your confirmation to confirm?")
 }
  function showPrompt() {
-    let input = prompt("Enter Your Name","Input Name");
+    const input = prompt("Enter Your Name","Input Name");
     if(input != null) {
         alert("Hello "+input+" jan!");
     }
  }
 
  function evenNums() {
-    let nums = [];
+    const nums = [];
 
     for(let i = 1; i <= 10; i++) {
         if(i%2==0) nums.push(i);
@@ -26,7 +26,7 @@ function showConfirm() {
  }
 
  function sorting() {
-    let arr = [3,2,30,-4,9,10,7,55,12];
+    const arr = [3,2,30,-4,9,10,7,55,12];
     let min, n=arr.length;
     for (i = 0; i < n - 1; i++)
 	{
@@ -81,9 +81,9 @@ function showConfirm() {
   }
   
   function divide() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 / num2;
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 / num2;
     document.getElementById("result").innerHTML = "Result: " + result;
 
     if (isNaN(num1) || isNaN(num2)) {
@@ -95,5 +95,57 @@ function showConfirm() {
         alert("Can't divide by zero,sorry man!");
         return;
     }
+  }
+
+  function calc(operator) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers!");
+        return;
+    }
+
+    if (num2 === 0) {
+        alert("Can't divide by zero,sorry man!");
+        return;
+    }
+    const result = eval(num1 +operator+ num2);
+    document.querySelector("#result").innerHTML = "Result: " + result;
+}
+
+
+
+
+  function calc(operator) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers!");
+        return;
+    }
+
+    if (num2 === 0) {
+        alert("Can't divide by zero,sorry man!");
+        return;
+    }
+    
+    let result;
+    switch(operator){
+      case '-':
+        result = num1 - num2;
+        break; 
+      case '/':
+        result = num1 / num2;
+        break; 
+      case '*':
+        result = num1 * num2;
+        break;
+      default:
+        result = num1 + num2;
+    }
+
+    document.querySelector("#result").innerHTML = "Result: " + result;
   }
   
